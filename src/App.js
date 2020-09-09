2be32d6db688aed2bb978479082295afae0f564f
mathew94 <mathewrodrick94@hotmail.com>
2020-09-09 15:04:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
+  const [randomUser, setRandomUser] = useState({});
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+
+    // singleUser
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.json())
+    .then(data => setSingleUser(data))
+
+    // randomUser
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUser(data.results[0]))
+  } ,[])
+  const handleLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AccessAlarmIcon></AccessAlarmIcon>
+      <ThumbUpAltIcon onClick = {handleLike} color = {likeColor}></ThumbUpAltIcon>
+      <h1>Name : {singleUser.name}</h1>
+      <h2>Random Gender : {randomUser.name && randomUser.name.title + randomUser.name.first + randomUser.name.last}</h2> 
+      {/* or i can use {randomUser.name?.title} */}
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
     </div>
   );
 }
